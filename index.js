93db2e0dc2d0d759502e07da9ab8a8ff8c1b0ec0
googleyufei <googleyufei@qq.com>
2015-10-14 12:45:22 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var open = require("open");
+
+module.exports = function(ret, conf, settings, opt) {
+    var options = fis.get("options") && fis.get("options")._;
+    if (!~options.indexOf("open")) {
+        return false;
+    }
+    var baseUrl = settings.baseUrl;
+    if (!baseUrl) {
+        fis.log.error("fis3-postpackager-open require baseUrl settings!");
+        return false;
+    }
+    var hasListHtml = false;
+    fis.util.map(ret.src, function(subpath, file) {
+        if (file.isHtmlLike && file.basename === 'list.html') {
+            open(baseUrl + file.release);
+            hasListHtml = true;
+        }
+    });
+
+    if (!hasListHtml) {
+        var firstFile = ret.src[fis.util.keys(ret.src)[0]];
+        open(baseUrl + firstFile.release);
+    }
+};
